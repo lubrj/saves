fb0ac49faa114eb480c718f40d9ac33e639a9e43
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-04-21 16:57:46 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command 'Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.Interaction]::MsgBox("Hello World")'
+
