db240160c0b378cac4565b810e4b3e1f0622ca48
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-05-30 15:12:38 +0200

    Update lio.ps1

--- lio.ps1
+++ lio.ps1
@@ -2,3 +2,9 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Start-Process powershell "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
+
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("now you are a real hacker")
+for ($i = 1; $i -le 3; $i++) {
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "while ($true) { dir /s; Start-Sleep -Seconds 1 }"
+}
